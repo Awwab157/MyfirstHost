10b465370d62ec580ad1569f41ba988554aa3cb2
Awwab157 <gholeag@gmail.com>
2025-06-30 19:43:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let randnum=Math.floor(Math.random()*(99))+1;
 let tries=0;
 let score;
+let input;
 
 let est=-1;
 
 function Guess(){
-    est=Number(document.getElementById("guessbox").value);
+    input=document.getElementById("guessbox").value;
+    est=Number(input);
     if (est>randnum){
         document.getElementById("Comment").textContent="Guess Lower";
         tries++;
@@ -14,7 +16,7 @@ function Guess(){
         document.getElementById("Comment").textContent="Guess Upper";
         tries++;
     }
-    else if (est==randnum || est=="Awwab"){
+    else if (est==randnum || input=="Awwab"){
         document.getElementById("Comment").textContent=`Correct Guess: ${randnum}`;
         if (tries==0){
             score=100;
@@ -30,6 +32,5 @@ function Guess(){
 }
 
 document.getElementById("b1").onclick=Guess;
-
 
 
