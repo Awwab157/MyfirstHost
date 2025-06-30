a3d7ffd3acb339342b3f784a263ccabd383ff94e
Awwab157 <gholeag@gmail.com>
2025-06-30 15:00:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+let randnum=Math.floor(Math.random()*(99))+1;
+let tries=0;
+let score;
+
+let est=-1;
+
+function Guess(){
+    est=Number(document.getElementById("guessbox").value);
+    if (est>randnum){
+        document.getElementById("Comment").textContent="Guess Lower";
+        tries++;
+    }
+    else if (est<randnum){
+        document.getElementById("Comment").textContent="Guess Upper";
+        tries++;
+    }
+    else {
+        document.getElementById("Comment").textContent=`Correct Guess: ${randnum}`;
+        if (tries==0){
+            score=100;
+        }
+        else{
+            score=100/tries;
+        }
+        document.getElementById("Score").textContent=`Your Score is: ${score}`;
+    }
+}
+
+document.getElementById("b1").onclick=Guess;
+
+
+
